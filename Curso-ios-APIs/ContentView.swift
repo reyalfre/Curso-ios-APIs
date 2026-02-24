73a3c21469f6373f6efd91a322b1a3b8fe51ce67
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-24 09:56:35 +0100

    Initial Commit

---
+++ Curso-ios-APIs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-ios-APIs
+//
+//  Created by Equipo 8 on 24/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-ios-APIs/Curso_ios_APIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_APIsApp.swift
+//  Curso-ios-APIs
+//
+//  Created by Equipo 8 on 24/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_APIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
