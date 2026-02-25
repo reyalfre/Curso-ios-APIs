9694cc7f2b032013bf30b6fff7ec67f8a04210db
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-25 13:47:26 +0100

    3 vistas añadidas, vistaFicheros, VistaThemes, VistaWebKit

---
+++ Curso-ios-APIs/VistaFicheros.swift
@@ -0,0 +1,18 @@
+//
+//  VistaFicheros.swift
+//  Curso-ios-APIs
+//
+//  Created by Equipo 8 on 25/2/26.
+//
+
+import SwiftUI
+
+struct VistaFicheros: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaFicheros()
+}
